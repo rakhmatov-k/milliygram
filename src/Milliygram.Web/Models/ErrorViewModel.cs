8285034c484ae860675e176214e0e950ac05da17
Javohir <javohirkhursanboyev@gmail.com>
2024-06-06 23:40:54 +0500

    Added Web Layer

---
+++ src/Milliygram.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Milliygram.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
