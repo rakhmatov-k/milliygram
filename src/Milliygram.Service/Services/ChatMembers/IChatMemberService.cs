aa4e5e962601f13d7648346b8ded3e27f96416d1
Javohir <javohirkhursanboyev@gmail.com>
2024-06-09 02:53:51 +0500

    Added ChatMemberService

---
+++ src/Milliygram.Service/Services/ChatMembers/IChatMemberService.cs
@@ -0,0 +1,13 @@
+﻿using Milliygram.Service.DTOs.ChatMembers;
+using X.PagedList;
+
+namespace Milliygram.Service.Services.ChatMembers;
+
+public interface IChatMemberService
+{
+    Task<ChatMemberViewModel> CreateAsync(ChatMemberCreateModel createModel);
+    Task<ChatMemberViewModel> UpdateAsync(long id, ChatMemberUpdateModel updateModel);
+    Task<bool> DeleteAsync(long id);
+    Task<ChatMemberViewModel> GetByIdAsync(long id);
+    Task<IPagedList<ChatMemberViewModel>> GetAllAsync(int? page, string search = null);
+}
