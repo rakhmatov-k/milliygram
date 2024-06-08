f3cadb4905742809b6c3eb3c697f71be9f58a9e0
Javohir <javohirkhursanboyev@gmail.com>
2024-06-09 03:05:51 +0500

    Added Build Solution

--- src/Milliygram.Domain/obj/Debug/net8.0/Milliygram.Domain.AssemblyInfo.cs
+++ src/Milliygram.Domain/obj/Debug/net8.0/Milliygram.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Milliygram.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+febf8c8e0445703542a402b537db7d1078d4edd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2306175cfc1b6512230b5d1d3c2973870e5a6527")]
 [assembly: System.Reflection.AssemblyProductAttribute("Milliygram.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Milliygram.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
