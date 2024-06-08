773fac3b02f9de2953dd8259da7756bd6d693e42
Javohir <javohirkhursanboyev@gmail.com>
2024-06-09 00:10:29 +0500

    1

--- src/Milliygram.Domain/obj/Debug/net8.0/Milliygram.Domain.AssemblyInfo.cs
+++ src/Milliygram.Domain/obj/Debug/net8.0/Milliygram.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Milliygram.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a40d47f08105186faae7270ae1302039d2cb25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26abeed0c4a049d082e1873ac60e982b9062b998")]
 [assembly: System.Reflection.AssemblyProductAttribute("Milliygram.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Milliygram.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Milliygram.Web/obj/Debug/net8.0/Milliygram.Web.AssemblyInfo.cs
+++ src/Milliygram.Web/obj/Debug/net8.0/Milliygram.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Milliygram.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465db82815c0d1b4d3738d74af602fcc050b702a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26abeed0c4a049d082e1873ac60e982b9062b998")]
 [assembly: System.Reflection.AssemblyProductAttribute("Milliygram.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Milliygram.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
